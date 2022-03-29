43844845d0c1bf246edeff30a66e537b29749dfb
Tristan Bilot <bilot.tristan@hotmail.fr>
2022-03-29 19:06:35 +0000

    ⚡️ add function to test model on fresh data

--- phishGNN/training.py
+++ phishGNN/training.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import itertools
 import json
+import time
 from collections import defaultdict
 from pprint import pprint
 
@@ -51,17 +52,61 @@ def test(model, loader, device):
     return correct / len(loader.dataset)
 
 
-def train(should_plot_embeddings: bool):
-    path = os.path.join(os.getcwd(), "data")
-    dataset = PhishingDataset(root=path, use_process=False, visulization_mode=False)
+def test_model(
+    weights_file: str,
+    use_process: bool=True,
+) -> float:
+    """Searches for crawled data as csv files in data/test/raw/
+    and then load and process a dataset on these data in order
+    to make predictions on every urls in that dataset.
+
+    Args:
+        weights_file: path from the weights folder to the pkl model
+        use_process: whether to run data process or not
+
+    Returns:
+        float: the accuracy of the model on these data
+    """
+    path = os.path.join(os.getcwd(), "data", "test")
+    dataset = PhishingDataset(root=path, use_process=use_process)
+
+    loader = DataLoader(dataset, batch_size=64, shuffle=False)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = torch.load(
+        os.path.join(os.getcwd(), "weights", weights_file),
+        map_location=torch.device('cpu'))
+    model.eval()
+
+    correct = 0
+    for data in loader:
+        out = model(data.x, data.edge_index, data.batch)
+        pred = out.argmax(dim=1)
+        correct += int((pred == data.y).sum())
+    
+    accuracy = correct / len(loader.dataset)
+    return accuracy
+
+
+
+def train(
+    should_plot_embeddings: bool=False,
+    use_process: bool=False,
+):
+    path = os.path.join(os.getcwd(), "data", "train")
+    dataset = PhishingDataset(root=path, use_process=use_process)
     dataset = dataset.shuffle()
 
-    train_test = 0.8
+    train_test = 0.9
     train_dataset = dataset[:int(len(dataset) * train_test)]
-    test_dataset = dataset[int(len(dataset) * train_test):]
+    test_dataset1 = dataset[int(len(dataset) * train_test):]
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    test_path = os.path.join(os.getcwd(), "data", "test")
+    test_dataset2 = PhishingDataset(root=test_path, use_process=False)
+    test_dataset = torch.utils.data.ConcatDataset([test_dataset1, test_dataset2])
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -90,7 +135,7 @@ def train(should_plot_embeddings: bool):
 
     lr = 0.01
     weight_decay = 4e-5
-    epochs = 1
+    epochs = 10
     
     accuracies = defaultdict(lambda: [])
     for (model, pooling, neurons) in itertools.product(
@@ -105,12 +150,14 @@ def train(should_plot_embeddings: bool):
             pooling_fn=pooling,
             device=device,
         )
+        model = model.to(device)
         label = f"{model.__class__.__name__}_{pooling.__name__}_{neurons}"
         print(f"\n{label}")
 
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
         loss_fn = torch.nn.CrossEntropyLoss()
 
+        start = time.time()
         train_accs, test_accs = [], []
         for epoch in range(epochs):
             if hasattr(model, 'fit'):
@@ -128,10 +175,12 @@ def train(should_plot_embeddings: bool):
             test_accs.append(test_acc)
             print(f'Epoch: {(epoch+1):03d}, Loss: {loss:.4f}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
 
+        end = time.time()
         accuracies[model.__class__.__name__].append({
             f'{pooling.__name__}, {neurons}': {
                 'train': mean_std_error(train_accs),
                 'test':  mean_std_error(test_accs),
+                'time': end - start,
             }
         })
 
@@ -153,8 +202,14 @@ def train(should_plot_embeddings: bool):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--training', action="store_true", help='if set, a training will be run from data/train/raw')
+    parser.add_argument('--test', action="store_true", help='if set, a test will be run from data/test/raw')
     parser.add_argument('--plot-embeddings', action="store_true",
         help='whether to save the embeddings in a png file during training or not')
     args, _ = parser.parse_known_args()
 
-    train(args.plot_embeddings)
+    if args.test is not None:
+        accuracy = test_model("20_epochs_default/ClusterGCN_global_max_pool_32.pkl")
+        print(accuracy)
+    else:
+        train(args.plot_embeddings)
