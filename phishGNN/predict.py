eedb9834a8a8884e60e96f7b29ac4db614651722
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-02-22 11:40:24 +0100

    fix predict.py

--- phishGNN/predict.py
+++ phishGNN/predict.py
@@ -8,14 +8,15 @@ from dataset_v1 import PhishingDataset
 from utils.compute_device import COMPUTE_DEVICE
 
 
-def predict(url: str, weights_file: str) -> int:
+def predict(weights_file: str) -> int:
     path = os.path.join(os.getcwd(), 'data', 'predict')
-    data_files = sorted(glob.glob(os.path.join(path, 'processed', '*')))
+    data_files = sorted(glob.glob(os.path.join(path, 'raw' '*')))
     if not os.path.exists(path) or len(data_files) == 0:
-        raise FileNotFoundError(f'No files found in path {path}, please the crawler before.')
+        raise FileNotFoundError(f'No files found in path {path}, please generate a csv dataset from the URLs you want to predict'
+            f' and move this file into {path}')
 
     dataset = PhishingDataset(root=path, do_data_preparation=True)
-    data = dataset[0]
+    data = dataset.data
     data = data.to(COMPUTE_DEVICE)
 
     model = torch.load(os.path.join(os.getcwd(), 'weights/', weights_file)).to(COMPUTE_DEVICE)
@@ -27,12 +28,11 @@ def predict(url: str, weights_file: str) -> int:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', type=str, help='the url to predict (phishing/benign)')
-    parser.add_argument('pkl_file', type=str, default='GCN_3_global_mean_pool_32.pkl',
+    parser.add_argument('--pkl_file', type=str, default='GCN_3_global_mean_pool_32.pkl',
                         help='the path to the model weights (.pkl)')
-    args, _ = parser.parse_known_args()
+    args = parser.parse_args()
 
-    pred = predict(args.url, args.weights_file)
+    pred = predict(args.pkl_file)
 
     if pred == 1:
         print('Phishing')
